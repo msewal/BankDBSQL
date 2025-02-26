2d78c3fd3f2241215ab986d5a302a1220e31048d
Melek Şevval ERDOĞAN <meleksevvalerdogan@gmail.com>
2025-02-26 03:29:51 +0300

    Contains credit card-related information

---
+++ data/Sikayet.sql
@@ -0,0 +1,7 @@
+INSERT INTO tblSikayet (SikayetID, MusteriID, SikayetTarihi, SikayetKonusu, SikayetDurumu)
+VALUES
+	(1, 1, '2023-03-01', 'ATM iþleminde para sýkýþmasý', 'Çözüm Bekliyor'),
+	(2, 4, '2023-03-02', 'Hesap özetimde hatalý iþlem görünüyor', 'Ýnceleniyor'),
+	(3, 9, '2023-03-03', 'Kredi baþvurum onaylanmadý', 'Reddedildi'),
+	(4, 10, '2023-03-04', 'Kartýmýn süresi doldu, yenilenmedi', 'Tamamlandý'),
+	(5, 46, '2023-03-05', 'Mobil bankacýlýk uygulamasýnda hata alýyorum', 'Ýnceleniyor');
